45ba50c6ea62a136d8df06513acf19700e32fb25
roscoe <roscoe@rindis-MacBook-Pro-2.local>
2021-01-13 19:10:10 -0500

    added comments explaining API process

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import Card from './components/Card';
 import Form from './components/Form';
 import Filter from './components/Sidebar';
@@ -9,136 +9,90 @@ import invisibilitylogo from "./InvisibilityLogo.png";
 
 
 class App extends Component {
-  constructor(props){
+  constructor(props) {
     super();
-    
-    this.state={
+
+    this.state = {
       entries: [],
-      isLoaded:false
+      isLoaded: false
     }
   }
 
-  callAPI(){
+  //callAPI only fetches (GET) data, it can't POST or PUT
+  //At the moment, it also gets ALL the data
+  callAPI() {
+    //replace localhost:2000 url with the URL where your database will live (e.g. http://robertglick.com/database/show)
+    //Alternatively, you can go to package.json, and set "proxy":"http://robertglick.com", and fetch("/show")
+    //You could write a function to randomize the this.state.entries array then store it in state for random card order, like in the original
     fetch("http://localhost:2000/show")
-    .then(response => response.json())
-    .then(response => {
-      this.setState({
-        isLoaded: true,
-        entries: response
-      });
-    })
-    .catch(err => {console.log(err)});
+      .then(response => response.json())
+      .then(response => {
+        this.setState({
+          isLoaded: true,
+          entries: response
+        });
+      })
+      .catch(err => { console.log(err) });
   }
 
   componentDidMount() {
-    // in my local environment, the server (PHP) is running on localhost:2000, while the frontend (React) is running on 3000
-    // localhost:2000 and localhost:3000 are treated as different domains, and this header and the header in the php
-    // document are telling the server that it's ok to request and send data from different domains (usually stopped by the CORS policy)
-
-    //replace localhost:2000 url with the URL where your php endpoint will live (e.g. http://robertglick.com/getData.php)
-    // if the domain name is different from where you're hosting this app, you'll need to replace the localhost:3000 address with the domain where this app lives
-    // if they're on the same domain you should be able to omit the Access-Control bits and the line in the PHP
+    /*
+     * Call the API (at the moment, it simply returns all of the results, not a specific number)
+     * In my local environment, two servers must be running. The Express server & the React server
+     * React is running on http://localthost:3000, while Express is running on http://localhost:2000
+     * To run the Express server, navigate to the-invisibility-project/src and run node server.js in the command line
+     * I don't need any special headers about access restrictions because I added the CORS library.
+     * To learn more about how this all works, please see src/server.js
+     */
     this.callAPI();
-    /*
-    this.setState({
-      isLoaded: true,
-      entries: data
-    });
-    console.log(data);
-    */
-
-    // read all entities
-    /*
-    const [associations, setAssociations] = React.useState(null);
-    fetch("http://localhost:2000/show", { 'Access-Control-Allow-Origin': '*',
-    'Access-Control-Allow-Credentials':'true'})
-    .then(response => console.log("Hello it's me ROSCOE"+Object.keys(response.json())))
-    .then(response => response.json())
-    .catch(err => {console.log(err)});
-    //.then(body => setAssociations(body));
-    
-    /*
-    .then(response => {
-      this.setState({
-        isLoaded: true,
-        entries: response
-      });
-    })
-    .catch(err => { console.log(err); 
-    });
-    */
-
-    /*
-    fetch("./data.json", { 'Access-Control-Allow-Origin': 'http://localhost:3000',
-    'Access-Control-Allow-Credentials':'true'})
-      .then(result => result.json())
-      .then(
-        (result) => {
-          this.setState({
-            isLoaded: true,
-            entries: result
-          });
-          console.log(result);
-        },
-        (error) => {
-          this.setState({
-            isLoaded: true,
-            error
-          });
-          console.log("There was an error loading the data");
-        }
-      )
-      */
   }
 
-  // you could write a function to randomize the this.state.entries array then store it in state for random card order, like in the original
-
   render() {
-    return(
-      
-       
-        
-    <div id="wrapperContainer">
-        <header>
-          <img src ={invisibilitylogo} height="120px" id = "logo" alt="logo" />
-          <p id ="instruct">Click on the tiles to reveal METATEXT</p>
+    return (
 
-            <div className="invisibleDot">
-                <button id="invisibleButton">[               .               ]</button>
-            </div>
-            
-         
+
+
+      <div id="wrapperContainer">
+        <header>
+          <img src={invisibilitylogo} height="120px" id="logo" alt="logo" />
+          <p id="instruct">Click on the tiles to reveal METATEXT</p>
+
+          <div className="invisibleDot">
+            <button id="invisibleButton">[               .               ]</button>
+          </div>
+
+
         </header>
-        
-        
-       
-        
+
+
+
+
         {/* A react quirk requires 'className' rather than 'class' like you'd find in HTML */}
         <div className="container">
-          {this.state.entries ? 
+          {this.state.entries ?
             //if true (entries exist in state), then map through the array of entries one by one and make a new Card element for each
-            this.state.entries.map((entry, index) =>{
+            this.state.entries.map((entry, index) => {
               return <Card key={index} quote={entry.Quote} author={entry.Author} source={entry.Text_source} />
             })
-          :
+            :
             //else, say:
             <p>No entries to display.</p>
           }
-          
+
         </div>
-     
-      
-      
-        <div id = "filters" className = "container">
-      
-            <div>
-                <Form />
-                <Filter />
-            </div>
-     
+
+
+
+        <div id="filters" className="container">
+
+          <div>
+            <Form />
+            <Filter />
+          </div>
+
         </div>
-      
-      
+
+
       </div>
     )
   }
--- src/server.js
+++ src/server.js
@@ -5,232 +5,209 @@ const https = require('https');
 const mongoClient = require('mongodb').MongoClient;
 var cors = require('cors');
 
-//I used the tutorial at https://rapidapi.com/blog/build-rest-api-node-js-express-mongodb/ to create this
+/*
+ * The way this whole thing works, is a little complicated, I used tutorials from the following:
+ * https://rapidapi.com/blog/build-rest-api-node-js-express-mongodb/
+ *  ^-- this site helped me build an Express.js backend to communicate with MongoDB
+ * https://rapidapi.com/blog/create-react-app-express/
+ *  ^-- this site helped me connect the backend(Express.js) with the frontend(React.js)
+ * https://www.freecodecamp.org/news/create-a-react-frontend-a-node-express-backend-and-connect-them-together-c5798926047c/
+ *  ^-- this site helped me troubleshoot problems I was having with CORS
+ */
+
+/*********** THE EXPLANATION ***********/
+/*
+ * Please consult the setup comments at componentDidMount() in src/App.js first
+ * If you want a more professional explanation, please consult https://rapidapi.com/blog/create-react-app-express/
+ * 
+ * 1. When someone opens the invisibility project page, the React frontend sends a GET request
+ *     to the Express.js backend. Express.js is built for backend and has libraries that can easily
+ *     handle API requests (GET, PUT, DELETE, etc.)
+ * 2. Express.js sees the React request (at /show for example), and then sends a corresponding 
+ *     request to the MongoDB database.
+ * 3. If this is a GET request, MongoDB then sends the information to the Express.js backend, which
+ *     is then updated as JSON form at the /show link (e.g. https://localhost:2000/show). Since this is
+ *     originally where the React frontend thought it was requesting data from, React takes that data 
+ *     and displays it as needed.
+ */
 
 //create basic server and implement handling different requests
+//change the port as needed
 const port = 2000;
-app.listen(port,function(){
+app.listen(port, function () {
     initialize();
     console.log(`listening on ${port}`);
 });
 
-function initialize(){
-    //by default, MongoDB is set up to port 27017 when you run it locally. This will be different when deploying the Db on a server
+function initialize() {
+    //By default, MongoDB is set up to port 27017 when you run it locally. I assume this will 
+    //be different when deploying the Db on a server
     const uri = "mongodb://127.0.0.1:27017";
-    
-    const client = new mongoClient(uri, {useNewUrlParser:true, useUnifiedTopology:true});
+
+    const client = new mongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 
     client.connect(err => {
-        if (err){
+        if (err) {
             console.log("error");
             console.log(err);
             client.close();
         }
-        //Using Express.js 
-        else{
+        else {
             console.log("connected to db");
-            const geoCollection = client.db("invis1").collection("invis_test_2");
+
+            //Create an instance of the the MongoDB collection
+            const invisCollection = client.db("invis1").collection("invis_test_2");
+
+            //cors() allows us to verify requests between frontent and backend
             app.use(cors());
-            app.post('/', function(req,res){
+
+            //retrieves all data from the database
+            app.get('/show', function (req, res, next) {
+                console.log("in GET");
+                handleShow(res, invisCollection);
+            });
+
+            //Work in progress
+            app.post('/', function (req, res) {
                 console.log("in POST");
                 const parsedURL = url.parse(req.url, true);
-                console.log(req.query.author+" | "+req.query.source+" | "+req.query.quote);
-                handleCreate(req.query.author,req.query.source,req.query.quote,res,geoCollection); 
+                console.log(req.query.author + " | " + req.query.source + " | " + req.query.quote);
+                handleCreate(req.query.author, req.query.source, req.query.quote, res, invisCollection);
             });
 
-            app.get('/show',function(req,res,next){
-                console.log("in GET");
-                handleShow(res,geoCollection);
-                /*
-                res.setHeader('content-type','Application/json');
-                res.setHeader('Access-Control-Allow-Origin','*');
-                res.setHeader('Access-Control-Allow-Credentials','true');
-                res.statusCode = 200;
-                //create an array from the map
-                console.log("sending...");
-                res.json([{"Author":"Amelia","Text_source":"Earhart","Quote":"In photography, if you are constantly moving in a long exposure, you are rendered essentially invisible. "}]);
-                */
-            });
+
         }
     });
-}
-
-function handleCreate(author,source,quote,res,dbCollection){
-    //call geolocation api and get the details
-            data = {"Author":author,"Text_source":source,"Quote":quote};
-            dbCollection.insertOne(data)
-            .then((result,error) => {
-                if(error){
-                    console.log(error);
-                }
-            }).catch(error => console.error("error"))
-            // set the header and status code success and return the details of the ip
-            res.set('content-type','Application/json');
-            res.statusCode = 200;
-            res.end(`record created: ${author}, ${source}, ${quote}`);
-}
-
-function handleShow(res,db){
-    db.find({},{projection:{_id:0}}).toArray()
-    .then(results =>{
-        //set the header and status
-        res.setHeader('content-type','Application/json');
-        res.setHeader('Access-Control-Allow-Origin','*');
-        res.setHeader('Access-Control-Allow-Credentials','true');
-        res.statusCode = 200;
-        //create an array from the map
-        res.send(JSON.stringify(results));
-    })
-    .catch(error => console.error(error))
 }
 
 /*
-function handleShow(res,db){
-    //db.find({},{projection:{_id:0}}).toArray()
-    //.then(results =>{
-        //set the header and status
-        res.setheader('content-type','Application/json');
-        res.setHeader('Access-Control-Allow-Origin','*');
-        res.setHeader('Access-Control-Allow-Credentials','true');
-        res.statusCode = 200;
-        //create an array from the map
-        console.log("sending...");
-        return res.send([{"Author":"Amelia","Text_source":"Earhart","Quote":"In photography, if you are constantly moving in a long exposure, you are rendered essentially invisible. "}]);
+ * Some important things to know for the following functions:
+ *
+ * res.statusCode is the HTTP status code. 200 means OK, 400 means Bad Request
+ * https://developer.mozilla.org/en-US/docs/Web/HTTP/Status
+ * 
+ * res.setHeader changes the header. Headers allow the frontend to know
+ * some extra infor such as content-type
+ * https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers
+ */
 
-    //})
-    //.catch(error => console.error(error))
-}
-*/
-
-function handleUpdate(ipAddress,res,db){
-    //call geolocation api and get the details
-    var query = {ip:ipAddress};
-    db.find(query,{projection:{_id:0}}).toArray()
-    .then(results => {
-        if(results.length > 0){
-            getGeolocation(ipAddress)
-            .then(response =>{
-                updateRecord(response,db);
-                res.setHeader('content-type','Application/json');
-                res.statusCode = 200;
-                var time = new Date();
-                var respJson = {"ip":response.ip,"country":response.country,"city":response.city,"lastUpdated":time}
-                res.end("record updated : "+ JSON.stringify(respJson));
-            },
-                error => {
-                    res.statusCode = 400;
-                    res.end(error);
-                })
-        }
-        else{
-            //ip not found
-            res.statusCode = 400;
-            res.send(`Read: ${ipAddress} not found`);
-        }
-    });
-}
-
-function handleRead(ipAddress,res,db){
-    var query = {ip: ipAddress};
-    db.find(query,{projection:{_id:0}}).toArray()
-    .then(results => {
-        if(results.lenth >0){
-            console.log("results:");
-            console.log(results);
-            res.setHeader('content-type','Application/json');
+function handleShow(res, collection) {
+    //returns all of the data in the collection, as denoted by the empty curly braces
+    collection.find({}, { projection: { _id: 0 } }).toArray()
+        .then(results => {
+            //set the header and status
+            res.setHeader('content-type', 'Application/json');
             res.statusCode = 200;
+            //send the JSON data to be displayed and read by the frontend
             res.send(JSON.stringify(results));
-        }
-        else{
-            //ip not found
-            res.statusCode = 400;
-            res.send(`ReadL ${ipAddress} not found`);
-        }
-    });
+        })
+        .catch(error => console.error(error))
 }
 
-function handleDelete(ipAddress,res,db){
+function handleCreate(author, source, quote, res, collection) {
+    //call geolocation api and get the details
+    data = { "Author": author, "Text_source": source, "Quote": quote };
+    collection.insertOne(data)
+        .then((result, error) => {
+            if (error) {
+                console.log(error);
+            }
+        }).catch(error => console.error("error"))
+    // set the header and status code success and return the details of the ip
+    res.setHeader('content-type', 'Application/json');
+    res.statusCode = 200;
+    res.end(`record created: ${author}, ${source}, ${quote}`);
+}
+
+function handleUpdate(ipAddress, res, collection) {
+    //call geolocation api and get the details
+    var query = { ip: ipAddress };
+    collection.find(query, { projection: { _id: 0 } }).toArray()
+        .then(results => {
+            if (results.length > 0) {
+                getGeolocation(ipAddress)
+                    .then(response => {
+                        updateRecord(response, collection);
+                        res.setHeader('content-type', 'Application/json');
+                        res.statusCode = 200;
+                        var time = new Date();
+                        var respJson = { "ip": response.ip, "country": response.country, "city": response.city, "lastUpdated": time }
+                        res.end("record updated : " + JSON.stringify(respJson));
+                    },
+                        error => {
+                            res.statusCode = 400;
+                            res.end(error);
+                        })
+            }
+            else {
+                //ip not found
+                res.statusCode = 400;
+                res.send(`Read: ${ipAddress} not found`);
+            }
+        });
+}
+
+function handleRead(ipAddress, res, collection) {
+    var query = { ip: ipAddress };
+    collection.find(query, { projection: { _id: 0 } }).toArray()
+        .then(results => {
+            if (results.lenth > 0) {
+                console.log("results:");
+                console.log(results);
+                res.setHeader('content-type', 'Application/json');
+                res.statusCode = 200;
+                res.send(JSON.stringify(results));
+            }
+            else {
+                //ip not found
+                res.statusCode = 400;
+                res.send(`ReadL ${ipAddress} not found`);
+            }
+        });
+}
+
+function handleDelete(ipAddress, res, collection) {
     //check if ip is in the table
-    var query = {ip:ipAddress};
-    db.deleteOne(query,function(err,obj){
-        if(err){
+    var query = { ip: ipAddress };
+    collection.deleteOne(query, function (err, obj) {
+        if (err) {
             res.statusCode = 400;
             res.send(`Delete: ${ipAddress} not found`);
         }
         //n in results indicates the number of records deleted
-        if(obj.result.n == 0){
+        if (obj.result.n == 0) {
             res.statusCode = 400;
             res.send(`Delete: ${ipAddress} not found`);
         }
-        else{
+        else {
             res.statusCode = 200;
             res.send(`record deleted: ${ipAddress}`);
         }
     });
 }
 
-function getGeolocation( ipAddress ) {
-    // initilize http.rquest object
-    var req = https.request;
-    // initialize header with the required information to call geolocation api. 
-    var header = {
-            "x-rapidapi-host": "ip-geolocation-ipwhois-io.p.rapidapi.com",
-            "x-rapidapi-key": "f9591de09dmsh8ad03cfe6c5144ep1fcbadjsnce3f3695820f",
-            "useQueryString": true
-        };
-    // add the query string including the IP address
-    var query_string = { "ip" : ipAddress };
-    // set the options parameter
-    var options = {
-                    headers: header,
-                    query: query_string
-                };
-    // form the url
-    const url =  rapidAPIBaseUrl + ipAddress ;
-    return new Promise ( ( resolve, reject) => {
-                        
-            https.get( url, options, res  => {
-               
-                let data = "";
-                 //data is received in chunks, so uppend data as and when received
-                res.on( 'data', function(response) {
-                    data = data + response;
-                });
-                // handle error if any
-                res.on( 'error', function(err) {
-                    console.log("Error");
-                    console.log(err);
-                })
-                // if end of data return the final chunk
-                res.on( 'end', () => {
-                    resolve(  JSON.parse(data) ); 
-                });
-            });//Endn of http
-    }); //end of return promise
-}//End of getGeolocation
-
-function insertRecord(entry,geoCollection){
+function insertRecord(entry, collection) {
     // get current date to update last update the time
     var time = new Date();
     // add the entry to table ip is the key and country, city and last updated time are stored
-    data = {"ip":entry.ip,"country":entry.country,"city":entry.city,"lastUpdated":time}
-    geoCollection.insertOne(data)
-    .then((result,error) => {
-        if(error){
-            console.log(error);
-        }
-    }).catch(error => console.error("error"))
-    
+    data = { "ip": entry.ip, "country": entry.country, "city": entry.city, "lastUpdated": time }
+    collection.insertOne(data)
+        .then((result, error) => {
+            if (error) {
+                console.log(error);
+            }
+        }).catch(error => console.error("error"))
+
 }
 
-function updateRecord(entry,geoCollection){
+function updateRecord(entry, collection) {
     // get current date to update last updated time
     var time = new Date();
     // add the entry to table ip is the key and country, city and last updated time are stored
-    var query = {"ip":entry.ip}
-    data = { $set : {"ip":entry.ip,"country":entry.country,"city":entry.city,"lastUpdated":time}}
-    geoCollection.updateOne(query,data)
-    .then((result,error) => {
-    })
-    .catch(error => console.error("error"))
+    var query = { "ip": entry.ip }
+    data = { $set: { "ip": entry.ip, "country": entry.country, "city": entry.city, "lastUpdated": time } }
+    collection.updateOne(query, data)
+        .then((result, error) => {
+        })
+        .catch(error => console.error("error"))
 }
