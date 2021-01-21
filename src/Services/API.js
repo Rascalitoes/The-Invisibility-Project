db3354b4b5a7bdefff93adf65c79f2a27c066ccd
roscoe <roscoe@rindis-MacBook-Pro-2.local>
2021-01-21 18:50:30 -0500

    moved all API services to Services folder

---
+++ src/Services/API.js
@@ -0,0 +1,34 @@
+//callAPI only fetches (GET) data, it can't POST or PUT
+export function callAPI(quantity = "/show", keywords = "") {
+    //replace localhost:2000 url with the URL where your database will live (e.g. http://robertglick.com/database/show)
+    //Alternatively, you can go to package.json, and set "proxy":"http://robertglick.com", and fetch("/show")
+    //You could write a function to randomize the this.state.entries array then store it in state for random card order, like in the original
+    let queries = addQueries({ "qty": quantity, "keywords": keywords });
+    console.log(queries);
+    return fetch(`http://localhost:2000${queries}`)
+        .then(response => response.json())
+        .catch(err => {
+            console.log(err)
+            return {
+                isLoaded: false,
+                entries: []
+            };
+        });
+}
+
+
+//This function is used in callAPI and formats the queries for the url
+//The argument 'query' is a dictionary.
+function addQueries(query = {}) {
+    let returnString = "";
+    //check for elements in dictionary, if there are any, prep for queries by adding '?'
+    returnString += "?";
+    for (var q in query) {
+        if (query[q]) {
+            returnString += (q + "=" + query[q] + "&");
+        }
+    }
+    //Because my code is sloppy, delete the last '&' that was added
+    returnString = returnString.slice(0, -1);
+    return returnString;
+}
---
+++ src/Services/server.js
@@ -0,0 +1,239 @@
+const express = require('express');
+const app = express();
+const url = require('url');
+const https = require('https');
+const mongoClient = require('mongodb').MongoClient;
+var cors = require('cors');
+
+/*
+ * The way this whole thing works, is a little complicated, I used tutorials from the following:
+ * https://rapidapi.com/blog/build-rest-api-node-js-express-mongodb/
+ *  ^-- this site helped me build an Express.js backend to communicate with MongoDB
+ * https://rapidapi.com/blog/create-react-app-express/
+ *  ^-- this site helped me connect the backend(Express.js) with the frontend(React.js)
+ * https://www.freecodecamp.org/news/create-a-react-frontend-a-node-express-backend-and-connect-them-together-c5798926047c/
+ *  ^-- this site helped me troubleshoot problems I was having with CORS
+ */
+
+/*********** THE EXPLANATION ***********/
+/*
+ * Please consult the setup comments at componentDidMount() in src/App.js first
+ * If you want a more professional explanation, please consult https://rapidapi.com/blog/create-react-app-express/
+ * 
+ * 1. When someone opens the invisibility project page, the React frontend sends a GET request
+ *     to the Express.js backend. Express.js is built for backend and has libraries that can easily
+ *     handle API requests (GET, PUT, DELETE, etc.)
+ * 2. Express.js sees the React request (at /show for example), and then sends a corresponding 
+ *     request to the MongoDB database.
+ * 3. If this is a GET request, MongoDB then sends the information to the Express.js backend, which
+ *     is then updated as JSON form at the /show link (e.g. https://localhost:2000/show). Since this is
+ *     originally where the React frontend thought it was requesting data from, React takes that data 
+ *     and displays it as needed.
+ */
+
+//create basic server and implement handling different requests
+//change the port as needed
+const port = 2000;
+app.listen(port, function () {
+    initialize();
+    console.log(`listening on ${port}`);
+});
+
+function initialize() {
+    //By default, MongoDB is set up to port 27017 when you run it locally. I assume this will 
+    //be different when deploying the Db on a server
+    const uri = "mongodb://127.0.0.1:27017";
+
+    const client = new mongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+
+    client.connect(err => {
+        if (err) {
+            console.log("error");
+            console.log(err);
+            client.close();
+        }
+        else {
+            console.log("connected to db");
+
+            //Create an instance of the the MongoDB collection
+            const invisCollection = client.db("invis1").collection("invis_test_2");
+
+            //cors() allows us to verify requests between frontent and backend
+            app.use(cors());
+
+            //retrieves ALL data from the database
+            app.get('/show', function (req, res, next) {
+                console.log("in GET");
+                handleShow(res, invisCollection);
+            });
+
+            app.get('', function (req, res, next) {
+                console.log("in GET qty");
+                const parsedURL = url.parse(req.url,true);
+                console.log(parsedURL.query.qty);
+                console.log(parsedURL.query.keywords);
+                handleShowRand(res, invisCollection, parsedURL);
+            });
+
+            //Work in progress
+            app.post('/', function (req, res) {
+                console.log("in POST");
+                const parsedURL = url.parse(req.url, true);
+                console.log(req.query.author + " | " + req.query.source + " | " + req.query.quote);
+                handleCreate(req.query.author, req.query.source, req.query.quote, res, invisCollection);
+            });
+
+
+        }
+    });
+}
+
+/*
+ * Some important things to know for the following functions:
+ *
+ * res.statusCode is the HTTP status code. 200 means OK, 400 means Bad Request
+ * https://developer.mozilla.org/en-US/docs/Web/HTTP/Status
+ * 
+ * res.setHeader changes the header. Headers allow the frontend to know
+ * some extra infor such as content-type
+ * https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers
+ */
+
+function handleShow(res, collection, quantity = 0) {
+collection.find({}, { projection: { _id: 0 }}).toArray()
+        .then(results => {
+            //set the header and status
+            res.setHeader('content-type', 'Application/json');
+            res.statusCode = 200;
+            //send the JSON data to be displayed and read by the frontend
+            res.send(JSON.stringify(results));
+        })
+        .catch(error => console.error(error))
+}
+
+//$sample returns a random sample of documents back. If all documents are unique,
+//then there will be no repeats.
+function handleShowRand(res, collection, parsedURL) {
+    let quantity = Number(parsedURL.query.qty);
+    let keywords = parsedURL.query.keywords;
+    collection.aggregate([{$sample: {size: quantity}}]).toArray()
+        .then(results => {
+            //set the header and status
+            res.setHeader('content-type', 'Application/json');
+            res.statusCode = 200;
+            //send the JSON data to be displayed and read by the frontend
+            res.send(JSON.stringify(results));
+        })
+        .catch(error => console.error(error))
+}
+
+
+
+
+function handleCreate(author, source, quote, res, collection) {
+    //call geolocation api and get the details
+    data = { "Author": author, "Text_source": source, "Quote": quote };
+    collection.insertOne(data)
+        .then((result, error) => {
+            if (error) {
+                console.log(error);
+            }
+        }).catch(error => console.error("error"))
+    // set the header and status code success and return the details of the ip
+    res.setHeader('content-type', 'Application/json');
+    res.statusCode = 200;
+    res.end(`record created: ${author}, ${source}, ${quote}`);
+}
+
+function handleUpdate(ipAddress, res, collection) {
+    //call geolocation api and get the details
+    var query = { ip: ipAddress };
+    collection.find(query, { projection: { _id: 0 } }).toArray()
+        .then(results => {
+            if (results.length > 0) {
+                getGeolocation(ipAddress)
+                    .then(response => {
+                        updateRecord(response, collection);
+                        res.setHeader('content-type', 'Application/json');
+                        res.statusCode = 200;
+                        var time = new Date();
+                        var respJson = { "ip": response.ip, "country": response.country, "city": response.city, "lastUpdated": time }
+                        res.end("record updated : " + JSON.stringify(respJson));
+                    },
+                        error => {
+                            res.statusCode = 400;
+                            res.end(error);
+                        })
+            }
+            else {
+                //ip not found
+                res.statusCode = 400;
+                res.send(`Read: ${ipAddress} not found`);
+            }
+        });
+}
+
+function handleRead(ipAddress, res, collection) {
+    var query = { ip: ipAddress };
+    collection.find(query, { projection: { _id: 0 } }).toArray()
+        .then(results => {
+            if (results.lenth > 0) {
+                console.log("results:");
+                console.log(results);
+                res.setHeader('content-type', 'Application/json');
+                res.statusCode = 200;
+                res.send(JSON.stringify(results));
+            }
+            else {
+                //ip not found
+                res.statusCode = 400;
+                res.send(`ReadL ${ipAddress} not found`);
+            }
+        });
+}
+
+function handleDelete(ipAddress, res, collection) {
+    //check if ip is in the table
+    var query = { ip: ipAddress };
+    collection.deleteOne(query, function (err, obj) {
+        if (err) {
+            res.statusCode = 400;
+            res.send(`Delete: ${ipAddress} not found`);
+        }
+        //n in results indicates the number of records deleted
+        if (obj.result.n == 0) {
+            res.statusCode = 400;
+            res.send(`Delete: ${ipAddress} not found`);
+        }
+        else {
+            res.statusCode = 200;
+            res.send(`record deleted: ${ipAddress}`);
+        }
+    });
+}
+
+function insertRecord(entry, collection) {
+    // get current date to update last update the time
+    var time = new Date();
+    // add the entry to table ip is the key and country, city and last updated time are stored
+    data = { "ip": entry.ip, "country": entry.country, "city": entry.city, "lastUpdated": time }
+    collection.insertOne(data)
+        .then((result, error) => {
+            if (error) {
+                console.log(error);
+            }
+        }).catch(error => console.error("error"))
+
+}
+
+function updateRecord(entry, collection) {
+    // get current date to update last updated time
+    var time = new Date();
+    // add the entry to table ip is the key and country, city and last updated time are stored
+    var query = { "ip": entry.ip }
+    data = { $set: { "ip": entry.ip, "country": entry.country, "city": entry.city, "lastUpdated": time } }
+    collection.updateOne(query, data)
+        .then((result, error) => {
+        })
+        .catch(error => console.error("error"))
+}
