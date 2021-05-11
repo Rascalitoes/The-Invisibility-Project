786c10a6d138f18f6c66075d31111ae2116fc513
roscoe <roscoe@rindis-MacBook-Pro-2.local>
2021-05-11 10:25:34 -0400

    updating passwords to include emails

--- src/Services/credentials.js
+++ src/Services/credentials.js
@@ -1,5 +1,14 @@
 const User = {
-    username : "your username",
-    password : "the password"
+    username : "your mongo atlas username",
+    password : "your mongo atlas password"
 }
-module.exports = {User};
+
+const mailInfo = {
+    email : "addressToSendFrom@email.com",
+    password : "email password",
+    host : "SMTP host",
+    port : 587,
+    adminEmail : "adminsEmailForVetting@email.com"
+}
+
+module.exports = {User,mailInfo};
